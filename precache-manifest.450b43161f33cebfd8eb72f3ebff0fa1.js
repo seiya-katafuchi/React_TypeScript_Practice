b2eb766be60f106d19adb96a0dc8761a1f29fc8d
Seiya Katafuchi <katafuchi612@yahoo.co.jp>
2020-09-13 19:00:24 +0900

    Updates

---
+++ precache-manifest.450b43161f33cebfd8eb72f3ebff0fa1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "789540255adc45edd8499d26e257c063",
+    "url": "/react-typescript-primer/index.html"
+  },
+  {
+    "revision": "cbe470e4f3405990ab92",
+    "url": "/react-typescript-primer/static/css/main.ec3a7956.chunk.css"
+  },
+  {
+    "revision": "c7bc9d41e08b4f7a0e76",
+    "url": "/react-typescript-primer/static/js/2.5e752559.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-typescript-primer/static/js/2.5e752559.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbe470e4f3405990ab92",
+    "url": "/react-typescript-primer/static/js/main.3224d6a2.chunk.js"
+  },
+  {
+    "revision": "ef5bf3f9ebedf196f740",
+    "url": "/react-typescript-primer/static/js/runtime-main.b128c54d.js"
+  }
+]);
